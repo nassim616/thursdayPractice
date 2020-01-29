990f4b42b3d16ecefee59de9cd75e92808c1d501
nassim616 <57570416+nassim616@users.noreply.github.com>
2020-01-28 22:57:26 -0600

    Update First.java

--- src/First.java
+++ src/First.java
@@ -11,5 +11,8 @@ public class First {
         System.out.println("working w metin");
         System.out.println("hahah");
         
+        System.out.println("hey word");
+        System.out.println("hello");
+        
     }
 }
